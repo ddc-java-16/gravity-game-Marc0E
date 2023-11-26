55f23513e156bb53cd55eb76f6cd16fa02fe7d4c
Marc0E <mestradamonge@cnm.edu>
2023-11-25 19:30:01 -0700

    Java doc update

--- app/src/main/java/edu/cnm/deepdive/gravity/model/Projectile.java
+++ app/src/main/java/edu/cnm/deepdive/gravity/model/Projectile.java
@@ -8,6 +8,10 @@ import androidx.annotation.Nullable;
 import com.google.gson.annotations.Expose;
 import java.util.Objects;
 
+/***
+ * Represents a projectile within the game.
+ * Projectiles are objects fired from the player's ship to interact with game elements.
+ */
 public class Projectile {
 
   private static final int PROJECTILE_SIZE = 20;
@@ -26,18 +30,40 @@ public class Projectile {
   private boolean move;
   private static final String TO_STRING_FORMAT = "%1$s[key=%2$s, name=%3$s, move=%4$s]";
 
+  /***
+   * Retrieves the X-coordinate position of the projectile.
+   * @return The X-coordinate position of the projectile.
+   */
   public int getPositionX() {
     return positionX;
   }
 
+
+  /***
+   * Retrieves the Y-coordinate position of the projectile.
+   * @return The Y-coordinate position of the projectile.
+   */
   public int getPositionY() {
     return positionY;
   }
 
+  /***
+   * Retrieves the bounding box of the projectile.
+   * @return The Rect object representing the bounding box of the projectile.
+   */
   public Rect getProjectileBox() {
     return projectileBox;
   }
 
+  /***
+   * Constructs a Projectile object with specified parameters.
+   * @param positionX   The initial X-coordinate position of the projectile.
+   * @param positionY   The initial Y-coordinate position of the projectile.
+   * @param xVelocity   The initial velocity in the X direction.
+   * @param yVelocity   The initial velocity in the Y direction.
+   * @param gravity     The gravitational force affecting the projectile's movement.
+   * @param gameField   The game field in which the projectile exists.
+   */
   public Projectile(int positionX, int positionY, double xVelocity, double yVelocity, double gravity, GameField gameField) {
     // TODO: 11/13/23 xposition, yposition, gameFiled, xvelocity and yvelocity.
     this.xVelocity = xVelocity;
@@ -64,29 +90,41 @@ public class Projectile {
     //Log.d(getClass().getSimpleName(), projectileBox.toString());
   }
 
-
+  /***
+   * Checks if the current projectile intersects with an enemy.
+   * @param other The Rect object representing the bounding box of the enemies.
+   * @return {@code true} if there is an intersection; {@code false} otherwise.
+   */
   public boolean intersects(Rect other) {
     return projectileBox.intersect(other);
   }
 
-  public boolean detonate(Rect enemy) {
-    return projectileBox.intersect(enemy);
-  }
+//  public boolean detonate(Rect enemy) {
+//    return projectileBox.intersect(enemy);
+//  }
 
-  public void fire() {
-    if (gameField.getShip().canFire(projectileBox)) {
-      shipPosition = gameField.getShip().getPositionY();
-      double positionX =
-          xVelocity * flyingTime; // TODO: 11/2/23 Add a loop to increment the position.
-      double positionY =
-          (shipPosition + yVelocity) * flyingTime + (0.5 * gravity * Math.pow(flyingTime, 2));
-      projectileBox.inset((int) positionX,
-          (int) positionY); // FIXME: 11/9/23 Check if cast is the  best option for this.
-      updatePosition();
-    }
-  }
+//  public void fire() {
+//    if (gameField.getShip().canFire(projectileBox)) {
+//      shipPosition = gameField.getShip().getPositionY();
+//      double positionX =
+//          xVelocity * flyingTime; // TODO: 11/2/23 Add a loop to increment the position.
+//      double positionY =
+//          (shipPosition + yVelocity) * flyingTime + (0.5 * gravity * Math.pow(flyingTime, 2));
+//      projectileBox.inset((int) positionX,
+//          (int) positionY); // FIXME: 11/9/23 Check if cast is the  best option for this.
+//      updatePosition();
+//    }
+//  }
 
+  /***
+   * Updates the position of the projectile within the game field.
+   * Handles the projectile's movement based on its velocity and gravity.
+   */
   public void updatePosition() {
+    positionX += xVelocity;
+    positionY += yVelocity;
+    yVelocity += (gameField.getGravity())/10;
+    computeProjectileBox();
    // flyingTime += 0.1;
     //positionX += (int) (xVelocity * flyingTime);
     //positionY += (int) ((gameField.getShip().getPositionY() + yVelocity) * flyingTime + (0.5 * gameField.getGravity() * Math.pow(flyingTime, 2)))/10;
@@ -96,10 +134,6 @@ public class Projectile {
 //    positionY += yVelocity;
 //    yVelocity += (gameField.getGravity())/100;
 
-    positionX += xVelocity;
-    positionY += yVelocity;
-    yVelocity += (gameField.getGravity())/10;
-    computeProjectileBox();
   }
 
 
--- app/src/main/java/edu/cnm/deepdive/gravity/model/Ship.java
+++ app/src/main/java/edu/cnm/deepdive/gravity/model/Ship.java
@@ -2,21 +2,23 @@ package edu.cnm.deepdive.gravity.model;
 
 import android.annotation.SuppressLint;
 import android.graphics.Rect;
-import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import com.google.gson.annotations.Expose;
 import java.util.Objects;
 
+/***
+ * Represents the player's ship within the game.
+ * Manages ship movement, shooting, and interaction with game elements.
+ */
 public class Ship {
 
+  private static final String TO_STRING_FORMAT = "%1$s[key=%2$s, name=%3$s, fly=%4$s]";
+  private static final int SHIP_SIZE = 30;
   private final String key = null;
   private int xPosition;
   private int yPosition;
   private String name;
   private boolean fly;
-  private static final String TO_STRING_FORMAT = "%1$s[key=%2$s, name=%3$s, fly=%4$s]";
-  private static final int SHIP_SIZE = 30;
   private Rect shipBox;
   private GameField gameField;
   private Projectile projectile;
@@ -31,6 +33,13 @@ public class Ship {
   private double flyingTime;
   private double gravity;
 
+
+  /***
+   * Constructs a Ship object at a specified position within the game field.
+   * @param gameField The game field in which the ship exists.
+   * @param y         The Y-coordinate position of the ship.
+   * @param x         The X-coordinate position of the ship.
+   */
   public Ship(GameField gameField, int y, int x) {
     // FIXME: 11/9/23 Check if this is correct
     //ship = new Rect(0, gameField.getBoundingBox().top/2,0,0);
@@ -41,10 +50,14 @@ public class Ship {
   }
 
   private void computeShipBox() {
-    shipBox = new Rect(positionX - SHIP_SIZE, positionY - SHIP_SIZE /2, positionX + SHIP_SIZE,
-        positionY + SHIP_SIZE/2);
+    shipBox = new Rect(positionX - SHIP_SIZE, positionY - SHIP_SIZE / 2, positionX + SHIP_SIZE,
+        positionY + SHIP_SIZE / 2);
   }
 
+  /**
+   * Moves the ship upwards within the game field. Adjusts the ship's position by moving it in the
+   * upward direction.
+   */
   public void moveUp() {
     if (positionY - 1 - SHIP_SIZE / 2 > gameField.getBoundingBox().top) {
       positionY -= 10;
@@ -52,6 +65,10 @@ public class Ship {
     }
   }
 
+  /**
+   * Moves the ship downwards within the game field. Adjusts the ship's position by moving it in the
+   * downward direction.
+   */
   public void moveDown() {
     if ((positionY + 1 - SHIP_SIZE / 2 < gameField.getBoundingBox().bottom)) {
       positionY += 10;
@@ -60,74 +77,102 @@ public class Ship {
   }
 
 
-
+  /***
+   * Checks if the current object's bounding box intersects with another rectangle (meteor).
+   * @param intersect The Rect object representing the meteor bounding rectangle.
+   * @return {@code true} if there is an intersection; {@code false} otherwise.
+   */
   public boolean intersects(Rect intersect) {
     return shipBox.intersect(intersect);
   }
 
+  /***
+   * Fires a projectile from the ship.
+   * @return The fired projectile object.
+   */
   public Projectile fire() {
-
     computeTrajectory(velocity);
-    Projectile projectile = new Projectile(positionX, positionY, xVelocity, yVelocity, gravity, gameField);
+    Projectile projectile = new Projectile(positionX, positionY, xVelocity, yVelocity, gravity,
+        gameField);
     return projectile;
   }
 
-  public boolean canFire(Rect project) {
-    return !project.isEmpty();
-  }
+//  public boolean canFire(Rect project) {
+//    return !project.isEmpty();
+//  }
 
 
+  /***
+   * Computes the trajectory of the projectile based on the given velocity.
+   * @param velocity The velocity of the projectile.
+   */
   public void computeTrajectory(double velocity) {
     shipPosition = shipBox.height();
     xVelocity = (velocity * Math.cos(Math.toRadians(angle)));
-    yVelocity = - (velocity * Math.sin(Math.toRadians(angle)));
+    yVelocity = -(velocity * Math.sin(Math.toRadians(angle)));
     //gravity = gameField.getMeteor().getGravity();
     totalFlyingTime =
         -yVelocity - (Math.sqrt(Math.pow(yVelocity, 2) - (4 * shipPosition * 0.5 * gravity))
             / gravity);
   }
 
-  public double getVelocity() {
-    return velocity;
-  }
+//  public double getVelocity() {
+//    return velocity;
+//  }
 
+  /***
+   * Sets the velocity value the ship will use for the projectile.
+   * @param velocity The velocity value to be set.
+   */
   public void setVelocity(double velocity) {
     this.velocity = velocity;
   }
 
-  public double getAngle() {
-    return angle;
-  }
+//  public double getAngle() {
+//    return angle;
+//  }
 
+  /***
+   * Sets the angle value the ship will use for the projectile.
+   * @param angle The angle value to be set.
+   */
   public void setAngle(double angle) {
     this.angle = angle;
   }
 
+  /***
+   * Sets the gravity value the ship will use for the projectile.
+   * @param gravity The gravity value to be set.
+   */
   public void setGravity(double gravity) {
     this.gravity = gravity;
   }
 
-  public int getPositionX() {
-    return positionX;
-  }
+//  public int getPositionX() {
+//    return positionX;
+//  }
 
-  public int getPositionY() {
-    return positionY;
-  }
+//  public int getPositionY() {
+//    return positionY;
+//  }
 
+  /***
+   * Retrieves the bounding box of the ship.
+   * @return The Rect object representing the bounding box of the ship.
+   */
   public Rect getShipBox() {
     return shipBox;
   }
 
-  public void setPositionY(int positionY) {
-    this.positionY = positionY;
-    computeShipBox();
-  }
-
-  public void setPositionX(int positionX) {
-    this.positionX = positionX;
-    computeShipBox();
-  }
+//  public void setPositionY(int positionY) {
+//    this.positionY = positionY;
+//    computeShipBox();
+//  }
+//
+//  public void setPositionX(int positionX) {
+//    this.positionX = positionX;
+//    computeShipBox();
+//  }
 
 
   @Override
